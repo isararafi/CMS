a77511e3d50a2efbd4cb2e0a4d8570c3d449fd93
muhammadshahzeb2782 <muhammadshahzeb2782@gmail.com>
2026-02-24 13:11:18 +0500

    added reducer

--- Client/src/app/store.js
+++ Client/src/app/store.js
@@ -1,9 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
+import adminReducer from '../features/auth/adminSlice';
+import teacherReducer from '../features/auth/teacherSlice';
+import studentReducer from '../features/auth/studentSlice';
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer
+    auth: authReducer,
+    admin: adminReducer,
+    teacher: teacherReducer,
+    student: studentReducer
   },
 });
 
